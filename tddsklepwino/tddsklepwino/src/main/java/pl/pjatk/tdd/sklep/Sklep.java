59de7ada85ffae875469480aae107248f8ffb1d4
Jan Ostrowski <s23820@pjwstk.edu.pl>
2021-10-19 11:18:44 +0200

    Zadanie 2.1

--- tddsklepwino/tddsklepwino/src/main/java/pl/pjatk/tdd/sklep/Sklep.java
+++ tddsklepwino/tddsklepwino/src/main/java/pl/pjatk/tdd/sklep/Sklep.java
@@ -3,8 +3,13 @@ package pl.pjatk.tdd.sklep;
 public class Sklep {
     public static void dokonajZakupu(KartaKlienta karta, double kwotaZakupu) {
         //TODO: dodaj algorytm na liczenie ilości kuponów jakie należą się za zakup
-        if (kwotaZakupu==40.00){
+
+        if(kwotaZakupu>=40&&kwotaZakupu<80){
             karta.dodajKupony(1);
+        }if(kwotaZakupu>=80&&kwotaZakupu<120){
+            karta.dodajKupony(2);
+        }if(kwotaZakupu>=120){
+            karta.dodajKupony(3);
         }
     }
 
